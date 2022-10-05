51c51b02a18763271331ba4d0d32977afb09fd60
Kineus <108695044+Kineus@users.noreply.github.com>
2022-10-05 16:37:04 +0100

    Initial Coin Offering

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x19bd9bfeBa51686C454eF87b9337FC64814D0638";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
